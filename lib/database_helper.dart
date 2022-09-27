ceb049bd57c71cf429810d9ca12ff91ae7cad553
Nabin <ndhakal10@gmail.com>
2022-09-27 15:12:37 +0545

    Updates

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -2,7 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart' as sql;
 
 class DatabaseHelper {
-  static Future<void> createTables(sql.Database database) async {
+  
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase(
+      'flutterjunction.db',
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+
+static Future<void> createTables(sql.Database database) async {
     await database.execute("""CREATE TABLE items(
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         title TEXT,
@@ -12,19 +24,8 @@ class DatabaseHelper {
       """);
   }
 // id: the id of a item
-// title, description: name and description of your activity
+// title, description: name and description of  activity
 // created_at: the time that the item was created. It will be automatically handled by SQLite
-
-  static Future<sql.Database> db() async {
-    return sql.openDatabase(
-      'nabindhakal.db',
-      version: 1,
-      onCreate: (sql.Database database, int version) async {
-        await createTables(database);
-      },
-    );
-  }
-
   // Create new item 
   static Future<int> createItem(String? title, String? descrption) async {
     final db = await DatabaseHelper.db();
@@ -33,6 +34,11 @@ class DatabaseHelper {
     final id = await db.insert('items', data,
         conflictAlgorithm: sql.ConflictAlgorithm.replace);
     return id;
+
+    // When a UNIQUE constraint violation occurs,
+    // the pre-existing rows that are causing the constraint violation
+    // are removed prior to inserting or updating the current row.
+    // Thus the insert or update always occurs.
   }
 
   // Read all items 
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,6 +11,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   // All data
   List<Map<String, dynamic>> myData = [];
+  final formKey = GlobalKey<FormState>();
 
   bool _isLoading = true;
   // This function is used to fetch all data from the database
@@ -34,68 +35,98 @@ class _HomePageState extends State<HomePage> {
   // This function will be triggered when the floating button is pressed
   // It will also be triggered when you want to update an item
   void showMyForm(int? id) async {
-    if (id != null) {
-      // id == null -> create new item
+    // id == null -> create new item
       // id != null -> update an existing item
-      final existingData =
-          myData.firstWhere((element) => element['id'] == id);
+    if (id != null) {      
+      final existingData = myData.firstWhere((element) => element['id'] == id);
       _titleController.text = existingData['title'];
       _descriptionController.text = existingData['description'];
+    } else {
+      _titleController.text = "";
+      _descriptionController.text = "";
     }
 
     showModalBottomSheet(
         context: context,
         elevation: 5,
+        isDismissible: false,
         isScrollControlled: true,
         builder: (_) => Container(
-              padding: EdgeInsets.only(
-                top: 15,
-                left: 15,
-                right: 15,
-                // prevent the soft keyboard from covering the text fields
-                bottom: MediaQuery.of(context).viewInsets.bottom + 120,
-              ),
+            padding: EdgeInsets.only(
+              top: 15,
+              left: 15,
+              right: 15,
+              // prevent the soft keyboard from covering the text fields
+              bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+            ),
+            child: Form(
+              key: formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  TextField(
+                  TextFormField(
                     controller: _titleController,
+                    validator: formValidator,
                     decoration: const InputDecoration(hintText: 'Title'),
                   ),
                   const SizedBox(
                     height: 10,
                   ),
-                  TextField(
+                  TextFormField(
+                    validator: formValidator,
                     controller: _descriptionController,
                     decoration: const InputDecoration(hintText: 'Description'),
                   ),
                   const SizedBox(
                     height: 20,
                   ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      // Save new data
-                      if (id == null) {
-                        await addItem();
-                      }
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            foregroundColor: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Exit")),
+                      ElevatedButton(
+                        onPressed: () async {
+                          if (formKey.currentState!.validate()) {
+                            if (id == null) {
+                              await addItem();
+                            }
 
-                      if (id != null) {
-                        await updateItem(id);
-                      }
+                            if (id != null) {
+                              await updateItem(id);
+                            }
 
-                      // Clear the text fields
-                      _titleController.text = '';
-                      _descriptionController.text = '';
+                            // Clear the text fields
+                            setState(() {
+                              _titleController.text = '';
+                              _descriptionController.text = '';
+                            });
 
-                      // Close the bottom sheet
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(id == null ? 'Create New' : 'Update'),
+                            // Close the bottom sheet
+                            Navigator.pop(context);
+                          }
+                          // Save new data
+                        },
+                        child: Text(id == null ? 'Create New' : 'Update'),
+                      ),
+                    ],
                   )
                 ],
               ),
-            ));
+            )));
+  }
+
+  String? formValidator(String? value) {
+    if (value!.isEmpty) return 'Field is Required';
+    return null;
   }
 
 // Insert a new data to the database
@@ -116,9 +147,7 @@ class _HomePageState extends State<HomePage> {
   void deleteItem(int id) async {
     await DatabaseHelper.deleteItem(id);
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Successfully deleted!'),
-    backgroundColor:Colors.green
-    ));
+        content: Text('Successfully deleted!'), backgroundColor: Colors.green));
     _refreshData();
   }
 
@@ -132,32 +161,35 @@ class _HomePageState extends State<HomePage> {
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : myData.isEmpty?const Center(child:  Text("No Data Available!!!")):  ListView.builder(
-              itemCount: myData.length,
-              itemBuilder: (context, index) => Card(
-                color:index%2==0?Colors.green: Colors.green[200],
-                margin: const EdgeInsets.all(15),
-                child:ListTile(
-                    title: Text(myData[index]['title']),
-                    subtitle: Text(myData[index]['description']),
-                    trailing: SizedBox(
-                      width: 100,
-                      child: Row(
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.edit),
-                            onPressed: () => showMyForm(myData[index]['id']),
+          : myData.isEmpty
+              ? const Center(child: Text("No Data Available!!!"))
+              : ListView.builder(
+                  itemCount: myData.length,
+                  itemBuilder: (context, index) => Card(
+                    color: index % 2 == 0 ? Colors.green : Colors.green[200],
+                    margin: const EdgeInsets.all(15),
+                    child: ListTile(
+                        title: Text(myData[index]['title']),
+                        subtitle: Text(myData[index]['description']),
+                        trailing: SizedBox(
+                          width: 100,
+                          child: Row(
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () =>
+                                    showMyForm(myData[index]['id']),
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () =>
+                                    deleteItem(myData[index]['id']),
+                              ),
+                            ],
                           ),
-                          IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () =>
-                                deleteItem(myData[index]['id']),
-                          ),
-                        ],
-                      ),
-                    )),
-              ),
-            ),
+                        )),
+                  ),
+                ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () => showMyForm(null),
